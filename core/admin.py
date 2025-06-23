2704e1064de468949f7841d8bdd36135b0243edc
lemussistemas <lemus.sistemas@gmail.com>
2025-06-23 09:07:57 -0600

    admin y mas

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Cliente, Vehiculo, Trabajo, CambioAceite
+from .models import Cliente, Vehiculo, Trabajo, CambioAceite, Servicio, EstimadoReparacion, DetalleEstimado
 
 admin.site.register(Cliente)
 admin.site.register(Vehiculo)
 admin.site.register(Trabajo)
 admin.site.register(CambioAceite)
+admin.site.register(Servicio)
+admin.site.register(EstimadoReparacion)
+admin.site.register(DetalleEstimado)
